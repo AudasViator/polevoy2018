807a88b9fa9d45e7fa08208cc24bba7d43641a6c
prieran <prieran@ya.ru>
2018-03-15 10:11:29 +0300

    mend

--- build.win64.vc2015/build.bat
+++
@@ -1,6 +0,0 @@
-@rem set PATH="C:\Program Files (x86)\CMake 3.0\bin\";%PATH%
-call "%VS140COMNTOOLS%..\..\VC\vcvarsall.bat" x86
-
-call cmake.exe -G"Visual Studio 14 Win64" -DOpenCV_DIR="C:\misis\polevoy\OpenCV\build" ..
-msbuild polevoy_d_v.sln /property:Configuration=Debug /m
-@rem msbuild polevoy_d_v.sln /property:Configuration=Release /m
