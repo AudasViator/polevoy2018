f63c8341e23179895f12dcfeb5d6002fe5b11be2
prieran <prieran@ya.ru>
2018-03-31 22:56:23 +0300

    mend

--- vasin_a_a/prj/cv_third_lab/cv_third_lab.cpp
+++ vasin_a_a/prj/cv_third_lab/cv_third_lab.cpp
@@ -6,70 +6,63 @@ using namespace cv;
 using namespace std;
 
 int main() {
-	Mat sourceImage = imread("src.png", CV_LOAD_IMAGE_GRAYSCALE);
+	Mat src = imread("src.png", CV_LOAD_IMAGE_GRAYSCALE);
 
 	Mat hist;
-
 	int channels[] = { 0 };
-
-	float range_0[] = { 0, 255 };
-	const float *ranges[] = { range_0 };
-
+	float range[] = { 0, 255 };
+	const float *ranges[] = { range };
 	int hbins = 255;
 	int histSize[] = { hbins };
-
-	calcHist(&sourceImage, 1, channels, Mat(), hist, 1, histSize, ranges);
+	calcHist(&src, 1, channels, Mat(), hist, 1, histSize, ranges);
 
 	bool left = false;
 	bool right = false;
-
-	int quantileLeft = 0, quantileRight = 255;
+	int binLeft = 0, binRight = 255;
 	float sumLeft = 0, sumRight = 0;
 
 	for (int i = 0; i < hist.size[0]; i++) {
-
-		if (!left && sumLeft / (sourceImage.size[0] * sourceImage.size[1]) < 0.2) {
+		if (!left && sumLeft / (src.cols * src.rows) < 0.2) {
 			sumLeft += hist.at<float>(i);
-			quantileLeft = i;
-		}
-		else {
+			binLeft = i;
+		} else {
 			left = true;
 		}
 
-		if (!right && sumRight / (sourceImage.size[0] * sourceImage.size[1]) < 0.2) {
+		if (!right && sumRight / (src.cols * src.rows) < 0.2) {
 			sumRight += hist.at<float>(hist.size[0] - 1 - i);
-			quantileRight = hist.size[0] - 1 - i;
-		}
-		else {
+			binRight = hist.size[0] - 1 - i;
+		} else {
 			right = true;
 		}
 	}
 
-	uchar table[256];
-
+	uchar matchTable[256];
 	for (int i = 0; i < 256; ++i) {
-		if (i < quantileLeft) {
-			table[i] = (uchar)0;
+		if (i < binLeft) {
+			matchTable[i] = (uchar)0;
 		}
-		else if (i > quantileRight) {
-			table[i] = (uchar)255;
+		else if (i > binRight) {
+			matchTable[i] = (uchar)255;
 		}
 		else {
-			table[i] = (uchar)(255.0 / (quantileRight - quantileLeft) * (i - quantileLeft));
+			matchTable[i] = (uchar)(255.0 / (binRight - binLeft) * (i - binLeft));
+		}
+	}
+	
+	Mat dst(src.clone());
+	for (int i = 0; i < dst.rows; i++) {
+		for (int j = 0; j < dst.cols; j++) {
+			dst.at<uchar>(i, j) = matchTable[dst.at<uchar>(i, j)];
 		}
 	}
 
-	Mat combinedImages(sourceImage.rows, sourceImage.cols * 2, CV_8UC1);
-	sourceImage.copyTo(combinedImages(Rect(0, 0, sourceImage.cols, sourceImage.rows)));
+	Mat combinedImages(src.rows, src.cols * 2, CV_8U);
+	src.copyTo(combinedImages(Rect(0, 0, src.cols, src.rows)));
+	dst.copyTo(combinedImages(Rect(src.cols, 0, src.cols, src.rows)));
 
-	for (int i = 0; i < combinedImages.rows; i++) {
-		for (int j = 0; j < combinedImages.cols / 2; j++) {
-			combinedImages.at<uchar>(i, j + sourceImage.cols) = table[sourceImage.at<uchar>(i, j)];
-		}
-	}
-
-	imwrite("result/L_3.png", combinedImages);
 	imshow("L_3", combinedImages);
+	imwrite("result/L_3.png", combinedImages);
 
 	waitKey(0);
 
